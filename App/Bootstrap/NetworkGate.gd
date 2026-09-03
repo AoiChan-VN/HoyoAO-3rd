645df7595413b7be4a8082a9fc85093c6c7290cd
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-09-04 01:17:47 +0700

    Update NetworkGate.gd

--- App/Bootstrap/NetworkGate.gd
+++ App/Bootstrap/NetworkGate.gd
@@ -1,6 +1,7 @@
 class_name NetworkGate
 extends Node
 
+signal state_changed(new_state: NetworkState.State)
 signal gate_passed()
 signal gate_blocked()
 
@@ -11,17 +12,17 @@ func _ready() -> void:
     _connectivity_service = ConnectivityServiceImpl.new()
     add_child(_connectivity_service)
     _connectivity_service.connectivity_changed.connect(_on_connectivity_changed)
-    
-    # Bắt đầu kiểm tra ngay khi scene ready
     _connectivity_service.check_connectivity()
 
 func _on_connectivity_changed(new_state: NetworkState.State) -> void:
     _current_state = new_state
+    state_changed.emit(_current_state)
+
     match _current_state:
         NetworkState.State.VALIDATED:
             gate_passed.emit()
         _:
             gate_blocked.emit()
-            
+
 func get_current_state() -> NetworkState.State:
     return _current_state
