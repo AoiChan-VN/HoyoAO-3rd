4a95e16f1a6b333860c11ac05c5cdf23eae432b5
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-09-04 05:45:39 +0700

    Add SkillDefinition resource with properties

---
+++ App/Resources/Definitions/SkillDefinition.gd
@@ -0,0 +1,8 @@
+class_name SkillDefinition
+extends Resource
+
+@export var skill_id: StringName = &""
+@export var display_name: String = ""
+@export var cooldown_seconds: float = 1.0
+@export var stamina_cost: float = 5.0
+@export var damage_multiplier: float = 1.0 
