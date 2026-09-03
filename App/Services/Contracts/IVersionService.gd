07bb2c07af891a744385812da586d15ff9f18cd1
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-09-04 05:08:27 +0700

    Add IVersionService interface with version methods

---
+++ App/Services/Contracts/IVersionService.gd
@@ -0,0 +1,10 @@
+class_name IVersionService
+extends RefCounted
+
+func is_version_valid() -> bool:
+    push_error("IVersionService.is_version_valid() not implemented.")
+    return false
+
+func get_version() -> String:
+    push_error("IVersionService.get_version() not implemented.")
+    return "" 
