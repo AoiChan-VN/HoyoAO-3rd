869332213a24224397d7610aba07fea3f2b44fd1
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-09-04 06:16:44 +0700

    Delete App directory

--- App/Services/Contracts/IConnectivityService.gd
+++
@@ -1,7 +0,0 @@
-class_name IConnectivityService
-extends Node
-
-signal connectivity_changed(new_state: NetworkState.State)
-
-func check_connectivity() -> void:
-    push_error("IConnectivityService: check_connectivity() must be overridden by implementation.") 
